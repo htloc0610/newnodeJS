5329a30810d32c48842f8df401c52cfbf68531c4
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2024-09-23 12:55:34 +0700

    haiz

--- api.js
+++ api.js
@@ -12,6 +12,9 @@ exports.handler = async (event, context) => {
 
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(data),
   };
 };
